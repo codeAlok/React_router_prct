846996d1d87a2ec5875c14d5e51d7c1011b30ad6
alok <alok60541@gmail.com>
2022-12-12 23:09:39 +0530

    Navlink + style(hover + isActive)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,26 @@
-import { Link } from "react-router-dom"; // Link => to use Link other pages
+import { Link, NavLink } from "react-router-dom"; // Link => to Link other pages
+// NavLink => to use className="" in (can't use class in LInk , could lead to issue or sow adds)
 
 function Navbar() {
     return (
         <>
-            <ul>
-                <li> <Link to="/"> Home_link </Link> </li>
-                <li> <Link to="/about"> About_link </Link> </li>
+            <ul className="navbar">
+
+                <li> <NavLink
+                    style={({ isActive }) => { return { color: isActive ? 'green' : 'red' } }}
+                    className="nav-bar-link" to="/"> Home </NavLink> </li>
+                <li> <NavLink
+                    style={({ isActive }) => { return { color: isActive ? 'green' : 'red' } }}
+                    className="nav-bar-link" to="/about"> About </NavLink> </li>
 
                 {/* we use <Link> </Link> instead of <a> </a> to avoid reload whole page after click on anchor element */}
-                <li><a href="/"> Home_anchor </a></li>
-                <li><a href="/about"> About_anchor</a></li>
+                {/* <li><a href="/"> Home-a </a></li>
+                <li><a href="/about"> About-a</a></li> */}
 
                 {/* invalid link no route defined in app.js redirect on home page/404 */}
-                <li> <Link to="/contact">Contact us(missing)</Link></li>
+                <li> <NavLink
+                    style={({ isActive }) => { return { color: isActive ? 'green' : 'red' } }}
+                    className="nav-bar-link" to="/contact">Contact us</NavLink></li>
             </ul>
         </>
     )
