f2ff9642d1fad65a03e2b4b0af48d6f6dfd625cf
alok <alok60541@gmail.com>
2022-12-13 23:14:53 +0530

    navigate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+    const navigate = useNavigate();
+
     return (
         <>
             <h1>Home page</h1>
             <p>Welcome to my cool simple Home Page</p>
             <p>Get Knowledge and learn something here</p>
             <Link to="/about"> Go to About Us page</Link>
+
+            <br />
+            <button onClick={() => navigate('/about')}>Go to About Page</button>
         </>
     )
 }
